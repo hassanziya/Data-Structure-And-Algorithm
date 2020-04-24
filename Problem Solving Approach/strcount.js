853ba0863cccc09ae8fb18e96ece51d015db04b5
Hassan Ziya <hassan.ziyakhan86@gmail.com>
2020-04-24 23:57:00 +0530

    slight change

--- Problem Solving Approach/strcount.js
+++ Problem Solving Approach/strcount.js
@@ -2,14 +2,27 @@ function strCounts(str) {
   var result = {};
 
   for (var i = 0; i < str.length; i++) {
-    var char = str[i].toLowerCase();
-    if (result[char] > 0) {
-      result[char]++;
-    } else {
-      result[char] = 1;
+    var char = str[i];
+    if (isAlphaNumeric(char)) {
+      if (result[char] > 0) {
+        result[char]++;
+      } else {
+        result[char] = 1;
+      }
     }
   }
   return result;
 }
 
-console.log(strCounts("hello"));
+function isAlphaNumeric(char) {
+  var code = char.charCodeAt(0);
+  if (code >= 66 && code <= 90) {
+    return true;
+  } else if (code >= 97 && code <= 122) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(strCounts("Hello 123 guys! let's have some fun"));
